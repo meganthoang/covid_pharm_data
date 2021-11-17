e616a517fa1f225758b58e50b44f97733813a9ac
meganthoang <77313353+meganthoang@users.noreply.github.com>
2021-11-16 21:34:30 -0500

    updated preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -3,16 +3,18 @@
 
 # %% matplotlib inline
 # first, let's import all necessary modules & assign aliases
+import os
 import pandas as pd
 import numpy as np
-import os
 import seaborn as sns
 from matplotlib import pyplot as plt
+from sklearn.linear_model import LinearRegression
+import datetime as dt
 
 # Preprocessing
 # Find out what the current directory is
 print(os.getcwd())
-
+ 
 df = pd.read_csv("C:/Users/megan/OneDrive/Desktop/covid_pharm_data/data.csv", index_col = 0, header = [0, 1])
 df.head()
 
@@ -21,20 +23,22 @@ df.index = pd.to_datetime(df.index)
 companies = ['PFIZER', 'ASTRAZENECA', 'BIONTECH', 'MODERNA', 'NOVAVAX']
 
 # %%
+# make separate  dataframes for each company in companies
 pfizer = df["PFIZER"]
 pfizer.head()
-# %%
+
 astrazeneca = df["ASTRAZENECA"]
 astrazeneca.head()
-# %%
+
 biontech = df["BIONTECH"]
 biontech.head()
-# %%
+
 moderna = df["MODERNA"]
 moderna.head()
-# %%
+
 novavax = df["NOVAVAX"]
 novavax.head()
+
 
 # %%
 # make the correlation matrix
@@ -44,10 +48,35 @@ for i in companies:
 prices.corr()
 
 # %%
-# heatmap using seaborn
-fig = plt.figure(figsize=(10,8))
-mask = np.zeros_like(preço_ativos.corr())
-mask[np.triu_indices_from(mask)] = True
-sns.heatmap(preço_ativos.corr(), annot=True, cmap='Blues', linewidths=0.2, mask=mask, vmin=0, vmax=1)
-plt.xticks(fontsize=11)
-plt.yticks(fontsize=11);
+# heatmap using seaborn to illustrate correlations
+#sns.color_palette("mako", as_cmap=True)
+fig = plt.figure(figsize=(7,6))
+sns.heatmap(prices.corr(), annot=True, cmap='mako', linewidths=0.2, vmin=0, vmax=1)
+
+# %%
+# type(pfizer.Close)
+# set our variables -- use .to_numpy() to format as ndarray
+# pfizer.index = pd.to_datetime(pfizer.index)
+# x = pfizer.index.to_numpy()
+# type(x)
+# print(pfizer.index)
+# linear regression doesn't work on datetime data
+
+# pfizer.index = pd.to_datetime(pfizer.index)
+# pfizer.index.datetime.toordinal()
+# x = pfizer.index.to_julian_date().to_numpy()
+# type(x)
+# print(pfizer.index)
+
+x = moderna.Close.to_numpy()
+
+# %%
+y = pfizer.Close.to_numpy()
+type(y)
+print(pfizer.Close)
+
+
+# %%
+p = LinearRegression().fit([x, y])
+
+# %%
